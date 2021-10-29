b41b81b2e468b4eb2ac09b8f7c8caa14f9b335ed
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-10-29 13:12:30 +0200

    Created main.py

---
+++ python/main.py
@@ -0,0 +1,10 @@
+import string
+
+class main :
+    
+    def init(self, maximumNodes, minimumNodeFees) :
+        
+        self.clients = 0
+        self.mxNods = maximumNodes
+        self.minNodFees = minimumNodeFees
+        self.nodes = []
