c5313d27f926eb9907f08423c3b6a2e7d1faa775
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-10-30 11:03:24 +0200

    Created gui.hpp

---
+++ C++/gui/gui.hpp
@@ -0,0 +1,28 @@
+namespace GUIHPP {
+    
+    class guiDatas() {
+        
+        public :
+            
+            int x_size = 1920;
+            int y_size = 1080;
+            int gui_d_type = 2;
+            
+        private :
+            
+            
+            
+    };
+    class gui() {
+        
+        public :
+            
+            
+            
+        private :
+            
+            
+            
+    };
+    
+}
