a4a8e40b32e44da3eee2beb68d15bc2e894da9d6
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-10-30 15:40:15 +0200

    Created logs.cpp

---
+++ C++/logs/logs.cpp
@@ -0,0 +1,27 @@
+#include <std>
+
+#include "./logs.json"
+
+namespace logs {
+    
+    int showLogs(int logsId) {
+        
+        if (logsId == 1) {
+            
+            return (LOGS[])
+            
+        }
+        else if (logsId == 2) {
+            
+            
+            
+        }
+        else if (logsId == 3) {
+           
+            
+            
+        }
+        
+    };
+    
+}
