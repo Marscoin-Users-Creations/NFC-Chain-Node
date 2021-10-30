e0133465c8574d6295727bcac2c7d864cee47345
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-10-30 10:59:33 +0200

    Created internet.hpp

---
+++ C++/internet/internet.hpp
@@ -0,0 +1,15 @@
+namespace INTERNETHPP {
+    
+    class internetServer() {
+        
+        public :
+            
+            int datas = ""
+            
+        private :
+            
+            int network = []
+            
+    }
+    
+}
