bfde7762e3d516066d4b62c7b2e38f541d939d88
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-10-30 10:46:30 +0200

    Created blockchain.cpp

---
+++ C++/blockchain/blockchain.cpp
@@ -0,0 +1,49 @@
+#include "../internet/internet.cpp"
+#include "../internet/internet.hpp"
+#include "../wallet/wallet.cpp"
+#include "../wallet/wallet.hpp"
+
+namespace blockchain {
+    
+    int create_chain(int starting_with_blocks, string blocks, int minimum_fees) {
+        
+        if (starting_with_blocks == 1) {
+            
+            
+            
+        } else {
+            
+            
+            
+        }
+        
+    };
+    int verify_chain(int number) {
+        
+        
+        
+    };
+    
+    int create_block(string previous_block_hash, string fees, string block_message) {
+        
+        
+        
+    };
+    int verify_block(string hash) {
+        
+        
+        
+    };
+    
+    int create_nfc_transaction(string previous_tx_hash, string from, string to, string fees) {
+        
+        
+        
+    };
+    int verify_nfc_transaction(hash) {
+        
+        
+        
+    };
+    
+};
