44c199d967d85cbafc32de3372fa6d6bd728f29c
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-10-30 11:09:27 +0200

    Updated main.cpp

--- C++/main.cpp
+++ C++/main.cpp
@@ -12,6 +12,10 @@ namespace main {
     int INIT() {
         
         INTERNET.internetServer.listen(CONFIG["server"["ip"]], CONFIG["server"["port"]])
+        INTERNET.internetClient.connect(DATABASE["peers"[0 : CONFIG["client"["maxPeers"]]]])
+        INTERNET.internetClient.send("sync_chain")
+        BLOCKCHAIN.blocks = INTERNET.internetClient.recv()
+        BLOCKCHAIN.verified_chain = BLOCKCHAIN.verify_chain(BLOCKCHAIN.blocks)
         
     }
     
