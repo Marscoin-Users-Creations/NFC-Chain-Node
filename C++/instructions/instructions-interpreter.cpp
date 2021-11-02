d2bbb4158e5f9c81559c1d1b754dfea3205e497b
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-11-02 08:09:40 +0100

    Updated instructions-interpreter.cpp

--- C++/instructions/instructions-interpreter.cpp
+++ C++/instructions/instructions-interpreter.cpp
@@ -2,6 +2,39 @@
 
 namespace INSTRUCTIONS_INTERPRETER {
     
+    int FUNC(int datas) = new int;
+    int IF(int datas, int datas2) = if(datas) { datas2 };
+    int FOR(int datas1, int calcul, int datas2, int result) = for(datas1 calcul datas2) { result };
+    int ELSE(int datas) = else { datas };
+    int THEN = {"{"};
+    int END = {"}"};
+    int SMCOL = ";";
     
+    int DEL(int funcName) 
+        
+        funcName.delete();
+        
+    };
+    
+    int CNCT(int serverAddress) {
+        
+        internet.internetClient.connect(serverAddress);
+        
+    };
+    int RECV(int fromServer) {
+        
+        return (internet.internetClient.recv(fromServer));
+        
+    };
+    int SEND(int toClient, datas) {
+        
+        internet.internetServer.send(toClient, datas);
+        
+    };
+    int DWLD(int fromServers, datas) {
+        
+        internet.internetClient.send(fromServers, datas)
+        
+    };
     
 }
