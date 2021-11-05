c991d93a399293c73fd5531a2a1674f4544bce37
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-11-05 22:21:28 +0100

    Updated core.kt

--- Kotlin/core/core.kt
+++ Kotlin/core/core.kt
@@ -1,12 +1,12 @@
 class _CORE_ {
     
-    fun start(work_to_do: Int) {
+    fun start(work_to_do: UInt) {
         
         
         
     };
     
-    fun operating_system_security(security_encryption_algorithm: Int) {
+    fun operating_system_security(security_encryption_algorithm: UInt) {
         
         
         
@@ -23,12 +23,32 @@ class _CORE_ {
             
             if (datas[0 : 2] == "SND") {
                 
-                _INTERNET_.client.send(datas[4 : (datas.size - 1)]);
+                if (core_datas["internet"["client_crashed"]] == 0) {
+                    
+                    _INTERNET_.client.send(datas[4 : (datas.size - 1)]);
+                    core_datas["internet"["client_packets_received"]] += 1;
+                    
+                } else {
+                    
+                    _BACKUP_INTERNET_.client.send(datas[4 : (datas.size - 1)]);
+                    core_datas["internet"["backup_client_packets_received"]] += 1;
+                    
+                };
                 
             };
             if (datas[0 : 2] == "RCV") {
                 
-                _INTERNET_.received_datas.append(_INTERNET_.client.recv());
+                if (core_datas["internet"["client_crashed"]] == 0) {
+                    
+                    _INTERNET_.received_datas.append(_INTERNET_.client.recv());
+                    core_datas["internet"["client_packets_received"]] += 1;
+                    
+                } else {
+                    
+                    core_datas["internet"["backup_client_received_datas"]];
+                    core_datas["internet"["backup_client_packets_received"]] += 1;
+                    
+                };
                 
             };
             
@@ -39,7 +59,28 @@ class _CORE_ {
                 
                 if (core_datas["internet"["server_crashed"]] == 0) {
                     
+                    _INTERNET_.server.send(datas[4 : (datas.size - 1)]);
+                    core_datas["internet"["server_packets_sent"]] += 1;
                     
+                } else {
+                    
+                    _BACKUP_INTERNET_.server.send(datas[4 : (datas.size - 1)]);
+                    core_datas["internet"["backup_server_packets_sent"]] += 1;
+                    
+                };
+                
+            };
+            if (datas[0 : 2] == "RCV") {
+                
+                if (core_datas["internet"["server_crashed"]] == 0) {
+                    
+                    _INTERNET_.received_datas.append(_INTERNET_.server.recv());
+                    core_datas["internet"["server_packets_received"]] += 1;
+                    
+                } else {
+                    
+                    core_datas["internet"["backup_packets"]].append(_BACKUP_SERVER_.server.recv());
+                    core_datas["internet"["backup_server_packets_received"]] += 1;
                     
                 };
                 
