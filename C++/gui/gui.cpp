dffab04d0ddaf5fbfbd31f13832a6e5c2eb65663
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-11-05 19:37:10 +0100

    Updated gui.cpp

--- C++/gui/gui.cpp
+++ C++/gui/gui.cpp
@@ -1,6 +1,198 @@
 #include <std>
 
-int guiDatas = { "home": { "button": { "xSize": "", "ySize": "" }, "text": { "ch": "", "en": "Home", "fr": "Menu", "jp": "", "pt": "", "sp": "" } } };
+int guiDatas = {
+    
+    "buttons": {
+        
+        "console": { "xSize": "", "ySize": "" },
+        "file": { "xSize": "", "ySize": "" },
+        "home": { "xSize": "", "ySize": "" },
+        "settings": { "xSize": "", "ySize": "" },
+        "transactions": { "xSize": "", "ySize": "" }
+        
+    },
+    "texts": {
+        
+        "af": {
+            
+            "enabled": 0,
+            
+            "console": "",
+            "file": "",
+            "home": "",
+            "settings": "",
+            "transactions": ""
+            
+        },
+        "alb": {
+            
+            "enabled": 0,
+            
+            "console": "",
+            "file": "",
+            "home": "",
+            "settings": "",
+            "transactions": ""
+            
+        },
+        "ar": {
+            
+            "enabled": 0,
+            
+            "console": "",
+            "file": "",
+            "home": "",
+            "settings": "",
+            "transactions": ""
+            
+        },
+        "bg": {
+            
+            "enabled": 0,
+            
+            "console": "",
+            "file": "",
+            "home": "",
+            "settings": "",
+            "transactions": ""
+            
+        },
+        "ch": {
+            
+            "enabled": 0,
+            
+            "console": "",
+            "file": "",
+            "home": "",
+            "settings": "",
+            "transactions": ""
+            
+        },
+        "de": {
+            
+            "enabled": 0,
+            
+            "console": "",
+            "file": "",
+            "home": "",
+            "settings": "",
+            "transactions": ""
+            
+        },
+        "en": {
+            
+            "enabled": 1,
+            
+            "console": "Console",
+            "file": "File",
+            "home": "Home",
+            "settings": "Settings",
+            "transactions": "Transactions"
+            
+        },
+        "fr": {
+            
+            "enabled": 1,
+            
+            "console": "Console",
+            "file": "Fichier",
+            "home": "Menu",
+            "settings": "Parametres",
+            "transactions": "Transactions"
+            
+        },
+        "gr": {
+            
+            "enabled": 0,
+            
+            "console": "",
+            "file": "",
+            "home": "",
+            "settings": "",
+            "transactions": ""
+            
+        },
+        "is": {
+            
+            "enabled": 0,
+            
+            "console": "",
+            "file": "",
+            "home": "",
+            "settings": "",
+            "transactions": ""
+            
+        },
+        "it": {
+            
+            "enabled": 0,
+            
+            "console": "",
+            "file": "",
+            "home": "",
+            "settings": "",
+            "transactions": ""
+            
+        },
+        "jp": {
+            
+            "enabled": 0,
+            
+            "console": "",
+            "file": "",
+            "home": "",
+            "settings": "",
+            "transactions": ""
+            
+        },
+        "ka": {
+            
+            "enabled": 0,
+            
+            "console": "",
+            "file": "",
+            "home": "",
+            "settings": "",
+            "transactions": ""
+            
+        },
+        "pl": {
+            
+            "enabled": 0,
+            
+            "console": "",
+            "file": "",
+            "home": "",
+            "settings": "",
+            "transactions": ""
+            
+        },
+        "pt": {
+            
+            "enabled": 0,
+            
+            "console": "",
+            "file": "",
+            "home": "",
+            "settings": "",
+            "transactions": ""
+            
+        },
+        "sp": {
+            
+            "enabled": 0,
+            
+            "console": "",
+            "file": "",
+            "home": "",
+            "settings": "",
+            "transactions": ""
+            
+        }
+        
+    }
+    
+};
 
 namespace _GUI_ {
     
