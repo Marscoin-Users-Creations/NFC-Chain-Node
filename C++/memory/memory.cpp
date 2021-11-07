f5f3a7ad497d1c67129e340bc4a1bc9a963d3194
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-11-07 10:34:59 +0100

    Updated memory.cpp

--- C++/memory/memory.cpp
+++ C++/memory/memory.cpp
@@ -1,10 +1,13 @@
 #include <memory.h>
 
+uint node_port;
+uint node_rpc_port;
+
 namespace memory {
     
-    int define_memory_size(uint memory_bytes_size) {
+    int reset_datas(string function_name) {
         
-        #define MCL_MALLOC(memory_bytes_size)
+        
         
     };
     
