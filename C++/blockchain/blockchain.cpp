b26986f14212f1ce405bed2a9f3613a5d3fb6902
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-11-07 13:58:41 +0100

    Updated blockchain.cpp

--- C++/blockchain/blockchain.cpp
+++ C++/blockchain/blockchain.cpp
@@ -1,15 +1,34 @@
+#include <string>
+
+#include "../algorithms/algorithms.cpp"
 #include "../internet/internet.cpp"
-#include "../internet/internet.hpp"
 #include "../wallet/wallet.cpp"
-#include "../wallet/wallet.hpp"
 
 namespace blockchain {
+    
+    uint blocks_before_valid_block = 10;
+    uint chain_size = 0;
+    string unvalid_blocks = [];
     
     int create_chain(int starting_with_blocks, string blocks, int minimum_fees) {
         
         if (starting_with_blocks == 1) {
             
-            
+            chain_size = blocks.size();
+            if (blocks.size < blocks_before_valid_block) {
+                
+                for (blocks.size != 0) {
+                    
+                    unvalid_blocks.append(blocks[0]);
+                    blocks.remove(0);
+                    
+                };
+                
+            } else {
+                
+                
+                
+            };
             
         } else {
             
@@ -26,7 +45,21 @@ namespace blockchain {
     
     int create_block(string previous_block_hash, string fees, string block_message) {
         
+        string block = "{'prevhash'" +previous_block_hash +"}";
         
+        if (unvalid_blocks.size() < blocks_before_valid_block) {
+            
+            uint block_good = 0;
+            
+            block_good = verify_block(block);
+            unvalid_blocks.append(block);
+            
+        } else {
+            
+            valid_blocks.append(unvalid_blocks[0]);
+            unvalid_blocks.append(block);
+            
+        };
         
     };
     int verify_block(string hash) {
