f189e85f4249b39d830424cb03a884913d79f010
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-12-08 11:30:27 +0100

    Updated mha.cpp
    Updated the MarscoinHashingAlgorithm.cpp file

--- C++/algorithms/mha.cpp
+++ C++/algorithms/mha.cpp
@@ -1,4 +1,5 @@
-#define uint025 62^64
+#define uint025 70^64
+#define algo_final_characters [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ",", ".", ":", ";", "!", "?", "/", "*"]
 
 #include <string>
 
