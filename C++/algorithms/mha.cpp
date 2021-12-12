497f4efd53e7e6fa49849091e5b728a8281f64d9
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-12-12 21:50:01 +0100

    Updated mha.cpp

--- C++/algorithms/mha.cpp
+++ C++/algorithms/mha.cpp
@@ -1,39 +1,41 @@
-#define uint025 70^64
+#define uintamv 70^64
 #define algo_final_characters [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ",", ".", ":", ";", "!", "?", "/", "*"]
 
 #include <string>
 
-uint025 hash = "0000000000000000000000000000000000000000000000000000000000000000";
+uintamv hash = "0000000000000000000000000000000000000000000000000000000000000000";
 
 namespace mha {
     
-    unsigned int brute_force(uint025 hash) {
+    unsigned int brute_force(uintamv hash) {
         
         
         
     };
     unsigned int hash(string datas) {
         
+        unsigned int datasModifications = 0;
         
+        unsigned int algo_repetitions = {"one": 0, "two": 0, "three": 0, "four": 0, "five": 0, "six": 0, "seven": 0, "eight": 0, "nine": 0, "ten": 0, "eleven": 0, "twelve": 0};
         
-        if (datas.size() > 64) {
+        if (datas.size() > 128) {
             
-            unsigned int datasStringNumber = 0;
+            unsigned int stringNumber = 0;
             
             for (datas.size() != 0) {
                 
-                new string()
+                new uintamv("datas" +datasStringNumber);
                 
             }
             
-        } else if (datas.size < 64) {
+        } else if (datas.size < 128) {
             
-            for (datas.size() != 64) {
+            for (datas.size() != 128) {
                 
                 datas.append(0)
                 
             }
-            for () {
+            for (algo_repetitions["one"] != 64) {
                 
                 
                 
@@ -41,7 +43,7 @@ namespace mha {
             
         } else {
             
-            for () {
+            for (algo_repetitions["one"] != +) {
                 
                 
                 
