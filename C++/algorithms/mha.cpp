7af659b7a21ecc663286b5aad256b3346c134adc
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-12-13 10:40:23 +0100

    Updated mha.cpp

--- C++/algorithms/mha.cpp
+++ C++/algorithms/mha.cpp
@@ -12,11 +12,11 @@ namespace mha {
         
         
     };
-    unsigned int hash(string datas) {
+    unsigned int hash(string datas, unsigned int x_modifications) {
         
         unsigned int datasModifications = 0;
         
-        unsigned int algo_repetitions = {"one": 0, "two": 0, "three": 0, "four": 0, "five": 0, "six": 0, "seven": 0, "eight": 0, "nine": 0, "ten": 0, "eleven": 0, "twelve": 0};
+        unsigned int algo_repetitions = {"one": 0, "two": 0, "three": 0, "four": 0, "five": 0, "six": 0, "seven": 0, "eight": 0, "nine": 0, "ten": 0, "eleven": 0, "twelve": 0, "thirteen": 0, "fourteen": 0, "fifteen": 0, "sixteen": 0};
         
         if (datas.size() > 128) {
             
@@ -35,7 +35,37 @@ namespace mha {
                 datas.append(0)
                 
             }
-            for (algo_repetitions["one"] != 64) {
+            for (algo_repetitions["one"] != 1) {
+                
+                
+                
+            }
+            for (algo_repetitions["two"] != 2) {
+                
+                
+                
+            }
+            for (algo_repetitions["three"] != 4) {
+                
+                
+                
+            }
+            for (algo_repetitions["four"] != 8) {
+                
+                
+                
+            }
+            for (algo_repetitions["five"] != 16) {
+                
+                
+                
+            }
+            for (algo_repetitions["six"] != 32) {
+                
+                
+                
+            }
+            for (algo_repetitions["seven"] != 64) {
                 
                 
                 
@@ -43,11 +73,54 @@ namespace mha {
             
         } else {
             
-            for (algo_repetitions["one"] != +) {
+            for (algo_repetitions["one"] != 1) {
                 
                 
                 
-            }
+            };
+            for (algo_repetitions["two"] != 2) {
+                
+                
+                
+            };
+            for (algo_repetitions["three"] != 4) {
+                
+                
+                
+            };
+            for (algo_repetitions["four"] != 8) {
+                
+                
+                
+            };
+            for (algo_repetitions["five"] != 16) {
+                
+                
+                
+            };
+            for (algo_repetitions["six"] != 32) {
+                
+                
+                
+            };
+            for (algo_repetitions["seven"] != 64) {
+                
+                
+                
+            };
+            for (algo_repetitions["eight"] != 64) {
+                
+                if (algo_repetitions["eight"] == 0 || 2 || 4 || 6 || 8 || 10 || 12 || 14 || 16 || 18 || 20 || 22 || 24 || 26 || 30) {
+                    
+                    
+                    
+                } else if (algo_repetitions["eight"] == 1 || 3 || 5 || 7 || 9 || 11 || 13 || 15 || 17 || 19 || 21 || 23 || 25 || 27 || 29 || 31) {
+                    
+                    
+                    
+                };
+                
+            };
             
         };
         
